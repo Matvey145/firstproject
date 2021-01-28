f15def2d10e631b270e3a58ebb1894196cbc008c
Matvey145 <matvey.p05@mail.ru>
2021-01-28 17:19:48 +0600

    zadacha 1

--- Задание 1/module1.py
+++ Задание 1/module1.py
@@ -7,7 +7,7 @@ with open('Perepis.txt','r') as f:
         a=int(s)
         l=i.split()
         if (data2<a<data1) or (data2>a>data1)  :
-            print(l[0])
+            print(l[0],l[1],l[2],l[3])
             k+=1
 print('Кол-во этих жителей:',k)
 
