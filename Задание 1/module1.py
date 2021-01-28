2fb587195bcf76ca29d13f6398d5eaf20fcf767a
Matvey145 <matvey.p05@mail.ru>
2021-01-28 17:12:53 +0600

    zadacha 1

--- Задание 1/module1.py
+++ Задание 1/module1.py
@@ -1,13 +1,15 @@
 with open('Perepis.txt','r') as f:
     k=0
+    data1=int(input())
+    data2=int(input())
     for i in f:
         s=i[-5:]
         a=int(s)
         l=i.split()
-        if a<1978 :
+        if (data2<a<data1) or (data2>a>data1)  :
             print(l[0])
             k+=1
-print('Кол-во жителей:',k)
+print('Кол-во этих жителей:',k)
 
 
 
